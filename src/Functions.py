02b74a6311b3e96fc668ea54856262cee0536e0c
DungeonmaAsster <kolya.kotlyarenko@gmail.com>
2022-07-20 19:23:14 +0500

    Теперь бот умеет создавать архив с фото из папки пользователя, отсылать его, а после удалять

--- src/Functions.py
+++ src/Functions.py
@@ -3,6 +3,7 @@ import os
 import telebot
 import pathlib
 import os.path
+import zipfile
 
 from pathlib import Path
 from dotenv import load_dotenv
@@ -13,7 +14,8 @@ load_dotenv()
 mess_time = datetime.datetime.now()
 bot = telebot.TeleBot(os.getenv('SECRET_KEY'))
 
-b=bot.send_message
+b = bot.send_message
+
 
 def website(massage):
     markup = types.ReplyKeyboardMarkup()
@@ -32,7 +34,6 @@ def start(massage):
 
 
 def user_text(massage):
-
     markup_help = types.ReplyKeyboardMarkup()
     help = types.KeyboardButton("/Начать")
     markup_help.add(help)
@@ -41,7 +42,7 @@ def user_text(massage):
         b(massage.chat.id, 'И тебе привет', parse_mode='html')
     elif massage.text == "Зарегестрироваться":
         user_path = Path('data', f'telegram-{massage.chat.id}')
-        if os.path.exists(f'{user_path}') == True:
+        if os.path.exists(f'{user_path}'):
             b(massage.chat.id, 'Вы уже зарегестрированны', reply_markup=markup_help)
         else:
             path = Path('data', f'telegram-{massage.chat.id}')
@@ -53,7 +54,27 @@ def user_text(massage):
     elif massage.text == "Сохранить мои фото":
         b(massage.chat.id, 'Можешь скинуть своё фото и я его сохраню', parse_mode='html')
     elif massage.text == 'Получить мои фото':
-        b(massage.chat.id, 'Я не понимаю что ты хочешь', parse_mode='html')
+        user_path = Path('data', f'telegram-{massage.chat.id}')
+        archived_folders = [f"{user_path}"]
+        arch_name = "Telegram_photo" + ".zip"
+        delete_path = Path('Telegram_photo.zip')
+
+        def archivator(arch, folder_list, mode):
+            num = 0
+            z = zipfile.ZipFile(arch, mode, zipfile.ZIP_DEFLATED, True)
+            for add_folder in folder_list:
+                for root, dirs, files in os.walk(add_folder):
+                    for file in files:
+                        path = os.path.join(root, file)
+                        z.write(path)
+                        num += 1
+            z.close()
+        archivator(arch_name, archived_folders, "w")
+        file_to_send = open('E:/pythonProject1/Telegram_photo.zip', 'rb')
+        bot.send_document(massage.chat.id, file_to_send)
+        file_to_send.close()
+        os.remove(f'{delete_path}')
+        b(massage.chat.id, 'Вот все твои фото', parse_mode='html')
     else:
         b(massage.chat.id, 'Я не понимаю что ты хочешь', parse_mode='html')
 
