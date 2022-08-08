d2fecc18b7c7bcd09de0828a73e81caad3ef8c63
DungeonmaAsster <kolya.kotlyarenko@gmail.com>
2022-08-08 14:58:35 +0500

    Начата работа над системой альбомов, добавленна возможность назвать новый альбом, и у пользователя спрашивается коректность ввода

--- src/Functions.py
+++ src/Functions.py
@@ -9,90 +9,52 @@ from pathlib import Path
 from dotenv import load_dotenv
 from telebot import types
 
+
 load_dotenv()
 
 mess_time = datetime.datetime.now()
 bot = telebot.TeleBot(os.getenv('SECRET_KEY'))
 
-b = bot.send_message
 PARSE_MOD = 'html'
 markup = types.ReplyKeyboardMarkup()
+album_name = ''
 
 
-def add(*args):
+def add(*args):                 # TODO обсудить проблему дублирования кнопок, как я понял они так делают из-за это срани
     global markup
-    markup = types.ReplyKeyboardMarkup()
+    markup = types.KeyboardButton()
 
     for i in args:
         markup.add(i)
-    markup.add(types.KeyboardButton('/Начать'))
+    markup.add(types.KeyboardButton('/Menu'))
 
 
-@bot.message_handler(commands=['start'])
-def start(massage):
-    add(types.KeyboardButton('Зарегестрироваться'))
-    b(massage.chat.id, f'Привет, {massage.from_user.first_name}, давай зарегестрируемся')
+def bot_send_message(telegram_object, message, parse_mode=PARSE_MOD):
+    bot.send_message(telegram_object.chat.id, message, parse_mode=parse_mode, reply_markup=markup)
 
 
-@bot.message_handler(commands=['Начать'])
-def website(massage):
-    Date = types.KeyboardButton("Дата и время")
-    get_photo = types.KeyboardButton('Получить мои фото')
-    save_photo = types.KeyboardButton('Сохранить мои фото')
-    markup.add(Date, get_photo, save_photo)
-    b(massage.chat.id, 'Выбери пункт', reply_markup=markup)
+def approved_album(message):
+    bot.send_message(message.chat.id, 'Да-да')
 
 
-def bot_send_message(massage, message, parse_mode=PARSE_MOD):
-    bot.send_message(massage.chat.id, message, parse_mode=parse_mode, reply_markup=markup)
+def create_new_album(message):
+    global album_name
+    album_name = message.text
+    key_yes = types.KeyboardButton('|Да.|')
+    key_no = types.KeyboardButton('|Нет.|')
+    markup.add(key_yes, key_no)
+    bot.send_message(message.chat.id, f'Вы назвали альбом как: {album_name}', reply_markup=markup)
+    # Проверка имени на корректность
+    # Существует ли уже такой альбом у пользователя
+    # Создаём альбом
+    # Сообщение о успешном создании альбома
+    # Перенаправление на меню
 
 
-@bot.message_handler(content_types=['text'])
-def user_text(massage):
-    text = 'Я не понимаю что ты хочешь'
-    if massage.text == "Привет":
-        text = 'И тебе привет'
-    elif massage.text == "Зарегестрироваться":
-        user_path = Path('data', f'telegram-{massage.chat.id}')
-        if os.path.exists(f'{user_path}'):
-            text = 'Вы уже зарегестрированны'
-        else:
-            path = Path('data', f'telegram-{massage.chat.id}')
-            os.mkdir(path)
-            text = 'Отлично, ты зарегестрирован, можешь начать работу со мной'
-    elif massage.text == "Дата и время":
-        text = f'Сегодняшняя дата и время: {mess_time}'
-    elif massage.text == "Сохранить мои фото":
-        text = 'Можешь скинуть своё фото и я его сохраню'
-    elif massage.text == 'Получить мои фото':
-        user_path = Path('data', f'telegram-{massage.chat.id}')
-        archived_folders = [f"{user_path}"]
-        arch_name = "Telegram_photo" + ".zip"
-        delete_path = Path('Telegram_photo.zip')
-
-        def archivator(arch, folder_list, mode):
-            num = 0
-            z = zipfile.ZipFile(arch, mode, zipfile.ZIP_DEFLATED, True)
-            for add_folder in folder_list:
-                for root, dirs, files in os.walk(add_folder):
-                    for file in files:
-                        path = os.path.join(root, file)
-                        z.write(path)
-                        num += 1
-            z.close()
-        archivator(arch_name, archived_folders, "w")
-        file_to_send = open('E:/pythonProject1/Telegram_photo.zip', 'rb')
-        bot.send_document(massage.chat.id, file_to_send)
-        file_to_send.close()
-        os.remove(f'{delete_path}')
-        text = 'Вот все твои фото'
-    bot_send_message(massage, text)
-
-
-@bot.message_handler(content_types=['photo'])
-def user_photo(massage):
-    file_info = bot.get_file(massage.photo[len(massage.photo) - 1].file_id)
-    path = Path('data', f'telegram-{massage.chat.id}')
-    with open(f'{path}\\' + massage.photo[0].file_id + '.jpg', 'wb') as file:
-        file.write(bot.download_file(file_info.file_path))
-    b(massage.chat.id, 'Фото сохранил', parse_mode='html')
+def archivate(arch, folders_list, mode):
+    zip = zipfile.ZipFile(arch, mode, zipfile.ZIP_DEFLATED, True)
+    for add_folder in folders_list:
+        for root, dirs, files in os.walk(add_folder):
+            for file in files:
+                zip.write(os.path.join(root, file))  # path = os.path.join(root, file)
+    zip.close()
--- src/main.py
+++ src/main.py
@@ -4,23 +4,91 @@ import telebot
 import os.path
 
 from dotenv import load_dotenv
-from Functions import website
-from Functions import start
-from Functions import user_text
-from Functions import user_photo
+from Functions import *
 
 load_dotenv()
 
 mess_time = datetime.datetime.now()
 bot = telebot.TeleBot(os.getenv('SECRET_KEY'))
+album_name = ''
 
 
-@bot.message_handler(commands=['start'], func=start)
-@bot.message_handler(commands=['Начать'], func=website)
-@bot.message_handler(content_types=['text'], func=user_text)
+@bot.message_handler(commands=['help'])
+def help_command(message):
+    bot_send_message(message, """
+    /help - справка
+/Menu - меню команд
+/new_album - создать альбом""")
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    add(types.KeyboardButton('Зарегестрироваться'))
+    bot_send_message(message, f'Привет, {message.from_user.first_name}, давай зарегестрируемся')
+
+
+@bot.message_handler(commands=['new_album'])
+def init_new_album(message):
+    bot.send_message(message.chat.id, 'Придумай название для альбома', parse_mode='html')
+    bot.register_next_step_handler(message, create_new_album)
+
+
+@bot.message_handler(commands=['Menu'])
+def website(message):
+    make_album = types.KeyboardButton("Создать альбом")
+    date = types.KeyboardButton("Дата и время")
+    get_photo = types.KeyboardButton('Получить мои фото')
+    save_photo = types.KeyboardButton('Сохранить мои фото')
+    markup.add(date, get_photo, save_photo, make_album)
+    bot_send_message(message, 'Выбери пункт')
+
+
+@bot.message_handler(func=lambda m: True)
+def user_text(message):
+    text = 'Я не понимаю что ты хочешь'
+    if message.text == "Привет":
+        text = 'И тебе привет'
+    elif message.text == "Зарегестрироваться":
+        user_path = Path('data', f'telegram-{message.chat.id}')
+        if os.path.exists(f'{user_path}'):
+            text = 'Вы уже зарегестрированны'
+        else:
+            os.mkdir(
+                path=Path('data', f'telegram-{message.chat.id}'))  # path = Path('data', f'telegram-{message.chat.id}')
+            text = 'Отлично, ты зарегестрирован, можешь начать работу со мной'
+    elif message.text == "Дата и время":
+        text = f'Сегодняшняя дата и время: {mess_time}'
+    elif message.text == "Сохранить мои фото":
+        text = 'Можешь скинуть своё фото и я его сохраню'
+    elif message.text == 'Получить мои фото':
+        archived_folders = [Path('data', f'telegram-{message.chat.id}')]
+        arch_name = "Telegram_photo" + ".zip"
+        archivate(arch_name, archived_folders, "w")
+        with open(f'E:/pythonProject1/{arch_name}', 'rb') as file:  # TODO путь абсоюлтный сделать!
+            bot.send_document(message.chat.id, file)
+        os.remove(Path(arch_name))
+        text = 'Вот все твои фото'
+    elif message.text == '|Да.|':
+        text = 'Да-да'
+    elif message.text == '|Нет.|':
+        text = 'Просто выбери один из вариантов'
+        key_new_name = types.KeyboardButton('/new_album')
+        key_another_func = types.KeyboardButton('/Menu')
+        markup.add(key_new_name, key_another_func)
+        bot.send_message(message.chat.id,
+                         'Ты можешь придумать иное название, или использовать другие функции',
+                         reply_markup=markup)
+
+    bot_send_message(message, text)
+
+
 @bot.message_handler(content_types=['photo'])
-def get_user_photo(massage):
-    user_photo(massage)
+def user_photo(message):
+    file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
+    path = Path('data', f'telegram-{message.chat.id}')
+    with open(f'{path}\\' + message.photo[0].file_id + '.jpg', 'wb') as file:
+        file.write(bot.download_file(file_info.file_path))
+    bot_send_message(message, 'Фото сохранил')
 
 
 bot.polling(none_stop=True)
