0b5bb4416046e5a9b08238bd571ef7f8a0aea351
DungeonmaAsster <kolya.kotlyarenko@gmail.com>
2022-08-10 20:57:22 +0500

    Исправил сообщение

--- src/main.py
+++ src/main.py
@@ -50,8 +50,7 @@ def download_to_album(message):
     if os.path.exists(Path('data', f'telegram-{message.chat.id}', f'{download_album_name}')):
         bot_send_message(message, f'Теперь можешь скинуть сюда свои фото и я сохраню их в {download_album_name}')
     else:
-        bot_send_message(message, 'Такого альбома не существует, проверте правильность ввода')
-#       bot.register_next_step_handler(message, download_to_album)
+        bot_send_message(message, 'Такого альбома не существует, проверте ')
 
 
 def upload_album(message):
